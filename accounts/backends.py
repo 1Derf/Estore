f9932f0b3f719b6037d85f66da74305ab3a88e58
derf <quaderone@pm.me>
2025-08-27 14:03:34 -0700

    A lot of changes. fixed bugs, got order confirmation emails working, added upload and download for products so bulk work can be done via spreadsheet, added admin honeypot fake sign in page to collect and block IP addresses, installed and got working Axes to track log ins and logouts along with failed attempts, also worked in admin panel to display info much better. this is just what I can remember.

---
+++ accounts/backends.py
@@ -0,0 +1,25 @@
+from django.contrib.auth.backends import ModelBackend
+from django.contrib.auth import get_user_model
+
+class CustomModelBackend(ModelBackend):
+    def authenticate(self, request, username=None, password=None, **kwargs):
+        UserModel = get_user_model()
+        if username is None:
+            username = kwargs.get(UserModel.USERNAME_FIELD)
+        try:
+            case_insensitive_username_field = '{}__iexact'.format(UserModel.USERNAME_FIELD)
+            user = UserModel._default_manager.get(**{case_insensitive_username_field: username})
+        except UserModel.DoesNotExist:
+            UserModel().set_password(password)  # Dummy check to prevent timing attacks
+            return None
+        else:
+            if user.check_password(password) and self.user_can_authenticate(user):
+                return user
+        return None
+
+    def get_user(self, user_id):
+        try:
+            user = get_user_model()._default_manager.get(pk=user_id)
+        except get_user_model().DoesNotExist:
+            return None
+        return user if self.user_can_authenticate(user) else None
