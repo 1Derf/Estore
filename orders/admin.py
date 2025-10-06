9cce5d1a6e23f284060a83c50c21587762c4c9a4
derf <quaderone@pm.me>
2025-10-06 16:45:11 -0700

    Got everything working now adding shipping info into the admin area.

--- orders/admin.py
+++ orders/admin.py
@@ -90,7 +90,7 @@ class OrderAdmin(admin.ModelAdmin):
                 "shipping_first_name", "shipping_last_name", "shipping_email", "shipping_phone",
                 "shipping_address_line_1", "shipping_address_line_2",
                 "shipping_city", "shipping_state", "shipping_zip_code", "shipping_country",
-                "shipping_method",
+                "shipping_method", 'shipping_carrier', 'custom_carrier_details',
             )
         }),
         ("Supplier Info (admin only)", {
